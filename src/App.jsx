5f9005c4042dd2f79391dc2f9320d8acd59933b4
Julian <julian.andreduerr@web.de>
2025-03-06 13:17:26 +0100

    added dynamic song collection rendering with styled cover images

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import "./App.css";
 
 // Temporary Imports
 import PlaybackControls from "./components/PlaybackControls/PlaybackControls";
+import { audioDatabase } from "../utils/AudioDatabase/AudioDatabase";
+import { useState } from "react";
+import SingleSong from "./components/SingleSong/SingleSong";
+import SongCollection from "./components/SongCollection/SongCollection";
 
 function App() {
+  // const [currentTrack, setCurrentTrack] = useState([]);
+  const [audioDB, setAudioDB] = useState(audioDatabase);
+
   return (
     <>
-      <div className="">
-        <PlaybackControls />
-      </div>
+      <PlaybackControls />
+      <SongCollection audioDB={audioDB} />
     </>
   );
 }
---
+++ utils/AudioDatabase/AudioDatabase.jsx
@@ -0,0 +1,38 @@
+export const audioDatabase = [
+  {
+    name: "Gibran Alcocer - Idea 1",
+    cover: "src/assets/cover/idea1.jpg",
+    path: "./audio/idea1.mp3",
+    id: crypto.randomUUID(),
+  },
+  {
+    name: "Gibran Alcocer - Idea 5",
+    cover: "src/assets/cover/idea5.jpg",
+    path: "./audio/idea9.mp3",
+    id: crypto.randomUUID(),
+  },
+  {
+    name: "Gibran Alcocer - Idea 9",
+    cover: "src/assets/cover/idea9.jpg",
+    path: "./audio/idea5.mp3",
+    id: crypto.randomUUID(),
+  },
+  {
+    name: "Gibran Alcocer - Idea 10",
+    cover: "src/assets/cover/idea10.jpg",
+    path: "./audio/idea10.mp3",
+    id: crypto.randomUUID(),
+  },
+  {
+    name: "Gibran Alcocer - Idea 15",
+    cover: "src/assets/cover/idea15.jpg",
+    path: "./audio/idea15.mp3",
+    id: crypto.randomUUID(),
+  },
+  {
+    name: "Gibran Alcocer - Idea 22",
+    cover: "src/assets/cover/idea22.jpg",
+    path: "./audio/idea22.mp3",
+    id: crypto.randomUUID(),
+  },
+];
