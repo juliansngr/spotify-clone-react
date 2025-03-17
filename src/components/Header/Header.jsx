7ef4150b3b29b338abdc204c5c3ae4a0e6aa70c5
Julian <julian.andreduerr@web.de>
2025-03-17 12:38:02 +0100

    added link to header logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,14 @@ import {
   SignInButton,
   UserButton,
 } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center p-5 bg-black">
-      <img src="../assets/logo.png" className="w-50 md:w-60" />
-
+    <header className="flex justify-between w-full items-center p-5 bg-black">
+      <Link to={"/"}>
+        <img src="../assets/logo.png" className="w-50 md:w-60 cursor-pointer" />
+      </Link>
       <SignedOut>
         <SignInButton className="px-5 py-2 md:px-6 md:py-3 text-l md:text-xl transition-all duration-500 rounded-4xl text-[#1e1e1e] bg-white hover:scale-105" />
       </SignedOut>
